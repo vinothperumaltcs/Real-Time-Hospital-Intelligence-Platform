8e55056b49bcf4c497dd6007da6802254b492172
vinothperumaltcs <152164465+vinothperumaltcs@users.noreply.github.com>
2026-06-05 16:23:14 +0530

    Bronze layer

---
+++ Bronze.py
@@ -0,0 +1,63 @@
+# Databricks notebook source
+from pyspark.sql.functions import *
+
+# Azure Event Hub Configuration
+event_hub_namespace = "<<Namespace_hostname>>"
+event_hub_name="<<Eventhub_Name>>"  
+event_hub_conn_str = "<<Connection_string>>"
+
+
+# COMMAND ----------
+
+kafka_options = {
+    'kafka.bootstrap.servers': f"{event_hub_namespace}:9093",
+    'subscribe': event_hub_name,
+    'kafka.security.protocol': 'SASL_SSL',
+    'kafka.sasl.mechanism': 'PLAIN',
+    'kafka.sasl.jaas.config': f'kafkashaded.org.apache.kafka.common.security.plain.PlainLoginModule required username="$ConnectionString" password="{event_hub_conn_str}";',
+    'startingOffsets': 'latest',
+    'failOnDataLoss': 'false'
+}
+
+# COMMAND ----------
+
+#Read from eventhub
+raw_df = (spark.readStream
+          .format("kafka")
+          .options(**kafka_options)
+          .load()
+          )
+
+#Cast data to json
+json_df = raw_df.selectExpr("CAST(value AS STRING) as raw_json")
+
+
+# COMMAND ----------
+#Cast data to json
+json_df = raw_df.selectExpr("CAST(value AS STRING) as raw_json")
+
+#ADLS configuration 
+spark.conf.set(
+  "fs.azure.account.key.<<Storageaccount_name>>.dfs.core.windows.net",
+  "<<Storage_Account_access_key>>"
+)
+
+bronze_path = "abfss://<<container>>@<<Storageaccount_name>>.core.windows.net/<<path>>"
+
+# COMMAND ----------
+
+
+# WRITE STREAM TO DELTA BRONZE
+query = (
+    json_df
+    .writeStream
+    .format("delta")
+    .outputMode("append")
+    .option("checkpointLocation", checkpoint_path)
+    .start(bronze_path)
+)
+
+
+# COMMAND ----------
+
+display(spark.read.format("delta").load(bronze_path))
